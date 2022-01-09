1308a55af1b37016ba67e1a5bbf99fb4fe91280b
loeaf <ckdic8524@naver.com>
2022-01-10 01:23:02 +0900

    이미지 업로드 기반 텍스트 아트 구현

--- src/app/pages/img-text-art/img-text-art.component.ts
+++ src/app/pages/img-text-art/img-text-art.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-img-text-art',
@@ -12,43 +12,22 @@ export class ImgTextArtComponent {
   asciiArtWidth = 80;
   customIntensityRamp: string = "@%#*+=-;. ";
   imageData: ImageData | undefined;
+  fontSize: string = '17px';
   constructor(private cdr: ChangeDetectorRef) {}
 
-  handleFileSelect(event: Event): void {
-    const fileInput = this.fileInput.nativeElement;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.setTextStyle();
+  }
 
-    if (fileInput.files && fileInput.files[0]) {
-      const file = fileInput.files[0];
-
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        const img = new Image();
-        img.onload = () => {
-          const canvas = document.createElement('canvas');
-          const context = canvas.getContext('2d');
-
-          if (context) {
-            const scaleFactor = this.maxCanvasWidth / img.width;
-            const scaledWidth = this.maxCanvasWidth;
-            const scaledHeight = img.height * scaleFactor;
-
-            canvas.width = scaledWidth;
-            canvas.height = scaledHeight;
-
-            context.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-            this.imageData = imageData;
-            this.asciiArt = this.processImage(imageData);
-          }
-        };
-
-        img.src = e.target?.result as string;
-      };
-
-      reader.readAsDataURL(file);
+  setTextStyle () {
+    const screenWidth = window.innerWidth;
+    const fontSize = Math.round(screenWidth / 55);
+    if (fontSize > 17) {
+      this.fontSize = '17px';
+      return;
     }
+    this.fontSize = `${fontSize}px`;
   }
 
   getMostIntenseColor(pixel: Uint8ClampedArray): number {
@@ -91,5 +70,41 @@ export class ImgTextArtComponent {
     this.asciiArt = this.processImage(this.imageData);
     // Trigger change detection manually
     this.cdr.detectChanges();
+  }
+
+  onFileSelected ($event: File) {
+    if ($event) {
+      const file = $event;
+
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
+
+          if (context) {
+            const scaleFactor = this.maxCanvasWidth / img.width;
+            const scaledWidth = this.maxCanvasWidth;
+            const scaledHeight = img.height * scaleFactor;
+
+            canvas.width = scaledWidth;
+            canvas.height = scaledHeight;
+
+            context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+            this.imageData = imageData;
+            this.asciiArt = this.processImage(imageData);
+          }
+        };
+
+        img.src = e.target?.result as string;
+        this.setTextStyle();
+      };
+
+      reader.readAsDataURL(file);
+    }
   }
 }
--- src/app/pages/img-text-art/img-text-art.module.ts
+++ src/app/pages/img-text-art/img-text-art.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImgTextArtComponent } from './img-text-art.component';
 import { FormsModule } from '@angular/forms';
+import { ImageUploaderModule } from '../../components/common/image-uploader/image-uploader.module';
 
 
 
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ImageUploaderModule
   ]
 })
 export class ImgTextArtModule { }
