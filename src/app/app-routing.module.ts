f47cd1d84e883edec3dd7f8f32b91a3d986f52eb
loeaf <ckdic8524@naver.com>
2023-12-22 22:23:06 +0900

    라우팅 모듈 수정

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { IntroComponent } from './pages/intro/intro.component';
 import { TextArtComponent } from './pages/text-art/text-art.component';
 import { ImgTextArtComponent } from './pages/img-text-art/img-text-art.component';
+import { EmojiKitchenComponent } from './pages/emoji-kitchen/emoji-kitchen.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/emoji-kitchen', pathMatch: 'full' },
   { path: 'intro', component: IntroComponent},
-  { path: 'text-art', component: TextArtComponent},
-  { path: 'img-text-art', component: ImgTextArtComponent}
+  { path: 'emoji-kitchen', component: EmojiKitchenComponent },
+  { path: 'ascii-emoji-generate' , component: HomeComponent },
+  { path: 'ascii-art', component: TextArtComponent},
+  { path: 'image-ascii-art', component: ImgTextArtComponent}
 ];
 
 @NgModule({
